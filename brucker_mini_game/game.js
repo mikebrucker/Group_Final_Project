130a8d16be7f73b41814cf3dc3e0561f33334877
Mike Brucker <disposition11@gmail.com>
2018-09-30 23:47:34 -0400

    empty game created

---
+++ brucker_mini_game/game.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade'
+    },
+    scene: {
+        key: 'main',
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
