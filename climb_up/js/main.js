955bf8e650c381de8569998679d5eb126af26ab0
Mario Borras <mario.borras@icloud.com>
2018-10-03 17:10:36 -0400

    update from Wednesday

--- climb_up/js/main.js
+++ climb_up/js/main.js
@@ -1,11 +1,16 @@
 let game;
 let cursor;
+let score;
 let soundOn = true;
 let musicOn = true;
 window.onload = function()
 {
     game=new Phaser.Game(480,640,Phaser.AUTO,"ph_game");
-	
+
+    game.state.add("StateTitle",StateTitle);
+    game.state.add("StateInstructions",StateInstructions);
+
     game.state.add("StateMain",StateMain);
-    game.state.start("StateMain");
+    game.state.add("StateOver",StateOver);
+    game.state.start("StateTitle");
 }
--- climb_up/js/stateTitle.js
+++ climb_up/js/stateTitle.js
@@ -1,15 +1,48 @@
-var StateTitle={    
+let StateTitle={    
     
-    preload:function()
-     {
+    preload:function(){
+        game.load.image("background","images/main/background.png");
+        game.load.spritesheet("buttons","images/ui/buttons.png",265,75);
+        game.load.image("traveler","images/main/opp_promo_traveler.png");
+        game.load.audio("backgroundMusic", "sounds/intro.mp3");
+
+
+
+
         
      },
      
-     create:function()
-     {
+     create:function(){
+        this.backgroundMusic = game.add.audio("backgroundMusic");
+        this.backgroundMusic.volume = .5;
+        this.backgroundMusic.loop = true;
+        this.backgroundMusic.play();
+
+        //the background
+        this.background = game.add.tileSprite(0,0,480,640,"background");
+        this.background.autoScroll(0,50);
          
+
+         // parameter is x, then y, the key, the function, the scope,the over normal and pressed states
+         this.traveler = game.add.sprite(game.world.centerX-150,game.world.centerY-250,"traveler");
+
+         this.titleText = game.add.text(game.world.centerX, 400, "Sky High", {
+            font: "50px Press Start 2P"  
+            , fill: "#ffffff"
+            , stroke: "#222222"
+            , strokeThickness: 4
+            , align: "center"
+        });
+        this.titleText.anchor.set(0.5, 0.5);
+         this.buttonStart = game.add.button(game.world.centerX,game.world.centerY+250,"buttons",this.play,this,6,7,6);
+         //center the button
+         this.buttonStart.anchor.set(0.5,0.5);
+
      },
-     
+     play:function() {
+        this.backgroundMusic.stop();
+        game.state.start("StateInstructions");
+     },
      update:function()
      {       
          
