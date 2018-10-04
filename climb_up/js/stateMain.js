6d78e8a572182383d5187d7f1cdfa43953843aa8
Mario Borras <mario.borras@icloud.com>
2018-10-04 15:00:12 -0400

    added wasp enemies and red special coins

--- climb_up/js/stateMain.js
+++ climb_up/js/stateMain.js
@@ -1,49 +1,44 @@
 const StateMain={    
     
     preload:function(){
-        game.load.image("background","images/main/background.png");
-        game.load.image("platform", "images/main/platform.png")
-        game.load.spritesheet("soundButtons", "images/ui/soundButtons.png", 44, 44, 4);
-        game.load.spritesheet("traveler", "images/main/traveler.png",64,64,33);
-        game.load.spritesheet("coins", "images/main/coins_all.png",15.875,16,8);
-        game.load.spritesheet("wasp", "images/main/wasp.png",96,96,11);
+         game.load.image("background","images/main/background.png");
+         game.load.image("platform", "images/main/platform.png")
+         game.load.spritesheet("soundButtons", "images/ui/soundButtons.png", 44, 44, 4);
+         game.load.spritesheet("traveler", "images/main/traveler.png",64,64,33);
+         game.load.spritesheet("coins", "images/main/coins_all.png",15.875,16,8);
+         game.load.spritesheet("wasp", "images/main/wasp.png",96,96,11);
+         game.load.audio("backgroundMusic", "sounds/background.mp3");
+         game.load.audio("jump", "sounds/jump.mp3");
+         game.load.audio("pick", "sounds/pickup.mp3");
+         game.load.audio("waspSound", "sounds/wasp.mp3");
 
-
-        game.load.spritesheet("bomb", "images/main/bomb.png",14,17,1);
-        game.load.audio("backgroundMusic", "sounds/background.mp3");
-        game.load.audio("jump", "sounds/jump.mp3");
-        game.load.audio("pick", "sounds/pickup.mp3");
-
-
-       
-     },
+    },
      
-     create:function(){
+    create:function(){
 
-        score = 0;
-
-        this.musicPlaying = false;
+         score = 0;
+        cursors = game.input.keyboard.createCursorKeys();
+         this.musicPlaying = false;
 
          //start the physics engine
          game.physics.startSystem(Phaser.Physics.ARCADE);
 
          //the player & animations
-        this.traveler = game.add.sprite(100,590,"traveler");
-        this.traveler.animations.add("run",[0,1,2,3,4,5],12,true);
-        this.traveler.animations.add("idle",[6,9,10,11,12,13],12,true);
-        this.traveler.animations.add("jump",[14,15,16,17,18,19,20,21,22,23],12,false);
-        this.traveler.animations.add("fall",[24,25,26,27,28,29,30,31,32],12,true);
+         this.traveler = game.add.sprite(100,590,"traveler");
+         this.traveler.animations.add("run",[0,1,2,3,4,5],12,true);
+         this.traveler.animations.add("idle",[6,9,10,11,12,13],12,true);
+         this.traveler.animations.add("jump",[14,15,16,17,18,19,20,21,22,23],12,false);
+         this.traveler.animations.add("fall",[24,25,26,27,28,29,30,31,32],12,true);
 
-        //sounds
-        this.jump = game.add.audio("jump");
-        this.pick = game.add.audio("pick");
-        this.backgroundMusic = game.add.audio("backgroundMusic");
-        this.backgroundMusic.volume = .5;
-        this.backgroundMusic.loop = true;
+         //sounds
+         this.jump = game.add.audio("jump");
+         this.pick = game.add.audio("pick");
+         this.waspsSound = game.add.audio("waspSound");
+         this.waspsSound.volume = .5;
 
-
-        cursors = game.input.keyboard.createCursorKeys();
-    
+         this.backgroundMusic = game.add.audio("backgroundMusic");
+         this.backgroundMusic.volume = .5;
+         this.backgroundMusic.loop = true;    
     
 
          //the background
@@ -55,6 +50,8 @@ const StateMain={
          this.platforms.createMultiple(40, "platform");
          this.platforms.setAll('checkWorldBounds', true);
          this.platforms.setAll('outOfBoundsKill', true);
+         this.platforms.scale.setTo(1.8);
+
 
          //the coins
          this.coins = game.add.group();
@@ -62,8 +59,8 @@ const StateMain={
          this.coins.setAll('checkWorldBounds', true);
          this.coins.setAll('outOfBoundsKill', true);
          this.coins.callAll('animations.add', 'animations', 'coin_rotate', [0,1,2,3,4,5,6,7], 12,true);
-        this.coins.callAll('play', null, 'coin_rotate');
-        this.coins.scale.setTo(1.8);
+         this.coins.callAll('play', null, 'coin_rotate');
+         this.coins.scale.setTo(1.8);
 
 
         //the wasps
@@ -76,21 +73,21 @@ const StateMain={
 
          
           //text
-          this.scoreText = game.add.text(game.world.centerX,60,"0");
-          this.scoreText.fill = "#000000";
-          this.scoreText.fontSize=64;
-          this.scoreText.anchor.set(0.5,0.5);
+         this.scoreText = game.add.text(game.world.centerX,60,"0");
+         this.scoreText.fill = "#000000";
+         this.scoreText.fontSize=64;
+         this.scoreText.anchor.set(0.5,0.5);
 
-          this.scoreLabel = game.add.text(game.world.centerX,20,"Score");
-          this.scoreLabel.fill = "#000000";
-          this.scoreLabel.fontSize=32;
-          this.scoreLabel.anchor.set(0.5,0.5);
+         this.scoreLabel = game.add.text(game.world.centerX,20,"Score");
+         this.scoreLabel.fill = "#000000";
+         this.scoreLabel.fontSize=32;
+         this.scoreLabel.anchor.set(0.5,0.5);
 
 
          //sound buttons
-        this.btnMusic = game.add.sprite(20, 20, "soundButtons");
-        this.btnSound = game.add.sprite(70, 20, "soundButtons");
-        this.btnMusic.frame = 2;
+         this.btnMusic = game.add.sprite(20, 20, "soundButtons");
+         this.btnSound = game.add.sprite(70, 20, "soundButtons");
+         this.btnMusic.frame = 2;
 
 
          //the ground
@@ -100,8 +97,9 @@ const StateMain={
          this.ground.enableBody = true;
 
 
-
+        //start of physics enabled
          game.physics.enable([this.traveler,this.platforms,this.ground,this.coins,this.wasps], Phaser.Physics.ARCADE);
+
          this.traveler.body.gravity.y = 500
          this.traveler.anchor.set(0.5,1);
          this.traveler.body.bounce.set(0.25);
@@ -110,12 +108,11 @@ const StateMain={
          
 
          this.ground.body.immovable = true;
-        //the platforms
+         //the platforms
          this.platforms.setAll('body.immovable', true);
          this.platforms.setAll("body.checkCollision.down", false);
          this.platforms.setAll("anchor.x", 0.5);
          this.platforms.setAll("anchor.y", 0.5);
-         this.platforms.scale.setTo(1.8);
          
          this.wasps.setAll('body.immovable', true);
          this.wasps.setAll("body.checkCollision.up", false);
@@ -124,8 +121,8 @@ const StateMain={
          
 
          
-                //bring the traveler above background
-                this.traveler.bringToTop();
+         //bring the traveler above background
+         this.traveler.bringToTop();
        
 
 
@@ -134,57 +131,72 @@ const StateMain={
          this.updateMusic();
          
 
-     },
-     setListeners:function() {
-        game.time.events.loop(Phaser.Timer.SECOND*3.5, this.launchPlatforms, this);
-        game.time.events.loop(Phaser.Timer.SECOND*4.5, this.launchCoins, this);
-        game.time.events.loop(Phaser.Timer.SECOND*3, this.launchWasps, this);
+    },
+    setListeners:function() {
+         game.time.events.loop(Phaser.Timer.SECOND*3.5, this.launchPlatforms, this);
+         game.time.events.loop(Phaser.Timer.SECOND*4.5, this.launchCoins, this);
+         game.time.events.loop(Phaser.Timer.SECOND*3, this.launchWasps, this);
 
 
-        this.ground.body.velocity.y = 2;
-        //enables the sprite to take inputs and then add the listener
-        this.btnSound.inputEnabled = true;
-        this.btnSound.events.onInputDown.add(this.toggleSound, this);
-        this.btnMusic.inputEnabled = true;
-        this.btnMusic.events.onInputDown.add(this.toggleMusic, this);
-
-
-     },
-     toggleSound: function () {
+         this.ground.body.velocity.y = 2;
+         //enables the sprite to take inputs and then add the listener
+         this.btnSound.inputEnabled = true;
+         this.btnSound.events.onInputDown.add(this.toggleSound, this);
+         this.btnMusic.inputEnabled = true;
+         this.btnMusic.events.onInputDown.add(this.toggleMusic, this);
+    },
+    toggleSound: function () {
         soundOn = !soundOn;
         this.updateButtons();
-     },
-     toggleMusic: function () {
+    },
+    toggleMusic: function () {
         musicOn = !musicOn;
         this.updateButtons();
         this.updateMusic();
     },
-     launchPlatforms: function(){
+    launchPlatforms: function(){
          let platform = this.platforms.getFirstDead();
         let xx = game.rnd.integerInRange(0,225);
          platform.reset(xx,0);
          platform.body.velocity.y = 30;
          console.log(xx,"platform fired")
 
-     },
-
-     launchCoins: function(){
-        let coin = this.coins.getFirstDead();
-       let xx = game.rnd.integerInRange(0,225);
-        coin.reset(xx,0);
-        coin.body.velocity.y = 100;
-        console.log(xx,"coin fired")
+    },
+    launchCoins: function(){
+         let coin = this.coins.getFirstDead();
+         let xx = game.rnd.integerInRange(0,225);
+         let specialCoin = game.rnd.integerInRange(1,2);
+         if (specialCoin == 1) {
+            coin.tint = 0xff0000;
+            coin.reset(xx,0);
+            coin.body.velocity.y = 200;
+         }else{
+            coin.reset(xx,0);
+            coin.body.velocity.y = 100;
+            //reset the tint for the group so that coin group doesnt have any tints
+            coin.tint = 0xffffff
+         }
 
     },
     launchWasps: function(){
         let wasp = this.wasps.getFirstDead();
-       let yy = game.rnd.integerInRange(10,580);
-        wasp.reset(0,yy);
-        wasp.body.velocity.x = 100;
-        console.log(yy,"wasp fired")
+        if (soundOn == true){
+        this.waspsSound.play();
+        };
+        let direction = game.rnd.integerInRange(1,2);
+        let yy = game.rnd.integerInRange(10,580);
+        if (direction == 1) {
+            wasp.scale.x = 1;
+            wasp.reset(0,yy);
+            wasp.body.velocity.x = 100;
+            }else {
+            wasp.scale.x = -1;
+            wasp.reset(460,yy);
+            wasp.body.velocity.x = -100;
+        }
 
     },
-     updateMusic: function () {
+    updateMusic: function () {
         if (musicOn == true) {
             if (this.musicPlaying == false) {
                 this.musicPlaying = true;
@@ -198,7 +210,7 @@ const StateMain={
             this.backgroundMusic.stop();
         }
     },
-     updateButtons: function () {
+    updateButtons: function () {
         if (soundOn == true) {
             this.btnSound.frame = 0;
         } else {
@@ -211,52 +223,59 @@ const StateMain={
         }
     },
     onCollect:function(traveler,coin){
+        if (coin.body.velocity.y >100) {
+            score=score+10
+          }else {
+          score++;
+          };
         coin.kill();
+        if (soundOn == true){
         this.pick.play();
-        score++;
+        };
+        
         this.scoreText.text = score;
         console.log("coin collected");
     },
-
-     update:function(){ 
-        game.physics.arcade.collide(this.traveler, this.ground);
-        game.physics.arcade.collide(this.traveler, this.platforms);
-        game.physics.arcade.collide(this.traveler, this.wasps);
-        game.physics.arcade.collide(this.traveler,this.coins,null,this.onCollect,this);
-        if (this.traveler.body.y == 576) {
+    update:function(){ 
+         game.physics.arcade.collide(this.traveler, this.ground);
+         game.physics.arcade.collide(this.traveler, this.platforms);
+         game.physics.arcade.collide(this.traveler, this.wasps);
+         game.physics.arcade.collide(this.traveler,this.coins,null,this.onCollect,this);
+         if (this.traveler.body.y == 576) {
             this.backgroundMusic.stop();
             game.state.start("StateOver");
-        }
+         }
             if (Math.abs(this.traveler.body.velocity.x) > 100){
                 this.traveler.animations.play("run");
             } else {
                 this.traveler.animations.play("idle");
-            }
+            };
         
         if (this.traveler.body.velocity.x > 0) {
             this.traveler.scale.x = 1;
         } else {
             this.traveler.scale.x = -1;
-        }
+        };
         
         if (cursors.left.isDown) {
             this.traveler.body.velocity.x = -250;
-        }
-        else if (cursors.right.isDown) {
+        } else if (cursors.right.isDown) {
             this.traveler.body.velocity.x = 250;
           //jump  
-        }else if(cursors.up.isDown){
+        } else if(cursors.up.isDown){
                 this.traveler.animations.play("jump");
                 if (this.traveler.body.touching.down){
                     this.traveler.body.velocity.y = -560;
-                    this.jump.play();
+                    if (soundOn == true){
+                        this.jump.play();
+                    }
                 }
             //stop
-         }else{
+         } else{
                 this.traveler.body.velocity.x = 0;
         }
         if (this.traveler.body.velocity.y > 150) {
             this.traveler.animations.play("fall");
         }
-     }     
+    }     
  }
